577bf45adbb9d13c03aa96d5421053e73f9a19f0
MenuraD <10899255@students.plymouth.ac.uk>
2024-11-13 17:11:06 +0530

    Imported a session

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, url_for, flash
+from flask import Flask, request, render_template, redirect, url_for, flash, session
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
