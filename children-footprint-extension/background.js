97b76b7e3dd185373673f0fbee1917c8e831aae7
MenuraD <10899255@students.plymouth.ac.uk>
2024-12-07 00:28:54 +0530

    Updated background.js file to collect and send data

--- children-footprint-extension/background.js
+++ children-footprint-extension/background.js
@@ -2,7 +2,7 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
     if (message.type === 'form_submission') {
         console.log("Email: " + message.email + " was registered at " + message.url);
         
-        // You can store this data locally or send it to a server
+        // Store the data locally
         chrome.storage.local.get({ registrations: [] }, function (data) {
             let registrations = data.registrations;
             registrations.push({
@@ -12,5 +12,26 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
             });
             chrome.storage.local.set({ registrations: registrations });
         });
+
+        // Send the data to the backend server
+        fetch("http://127.0.0.1:5000/api/track-email", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                email: message.email,
+                url: message.url,
+                timestamp: new Date().toISOString(),
+            }),
+        })
+        .then((response) => {
+            if (response.ok) {
+                console.log("Data sent to server successfully.");
+            } else {
+                console.error("Failed to send data to server. Status:", response.status);
+            }
+        })
+        .catch((err) => console.error("Error sending data to server:", err));
     }
 });
