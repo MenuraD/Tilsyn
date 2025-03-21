edbf8b9e6cacb6faa1fa42ad97c8f1cef4e1274b
MenuraD <10899255@students.plymouth.ac.uk>
2025-03-21 10:06:28 +0530

    Update  7

--- extension/content.js
+++ extension/content.js
@@ -13,23 +13,3 @@ document.addEventListener('submit', function (event) {
         });
     }
 });
-
-// Track manual login events (clicking a login button)
-document.addEventListener('click', function (event) {
-    if (event.target.tagName === 'BUTTON' || event.target.type === 'submit') {
-        const form = event.target.closest('form');
-        if (form) {
-            const emailInput = form.querySelector('input[type="email"]');
-            if (emailInput) {
-                const email = emailInput.value;
-                const url = window.location.href;
-
-                chrome.runtime.sendMessage({
-                    type: 'form_submission',
-                    email: email,
-                    url: url
-                });
-            }
-        }
-    }
-});
